6e25f1e4e1ab8bab51d6faf978b3bef64b2fc1af
egiby07 <egiby07@gmail.com>
2026-09-22 17:52:52 +0900

    Add free browser editor configuration

---
+++ assets/js/admin-config.js
@@ -0,0 +1,9 @@
+window.PORTFOLIO_ADMIN_CONFIG = {
+  // Google Cloud에서 발급한 Web OAuth Client ID를 입력하세요.
+  // 예: "1234567890-abcdefg.apps.googleusercontent.com"
+  googleClientId: "",
+  // 보안상 서버 검증을 할 수 없는 GitHub Pages의 무료 정적 방식입니다.
+  // 첫 Google 로그인 계정을 이 브라우저의 관리자 계정으로 등록합니다.
+  // 다른 브라우저/기기와 관리자 권한을 공유하지 않습니다.
+  localAdminBinding: true
+};
