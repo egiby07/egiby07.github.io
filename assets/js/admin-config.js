570c13b381582dcd2d577345f68396ae69c5ec0c
egiby07 <egiby07@gmail.com>
2026-09-22 17:53:06 +0900

    Restrict local admin to egiby07 Google account prefix

--- assets/js/admin-config.js
+++ assets/js/admin-config.js
@@ -1,9 +1,8 @@
 window.PORTFOLIO_ADMIN_CONFIG = {
-  // Google Cloud에서 발급한 Web OAuth Client ID를 입력하세요.
-  // 예: "1234567890-abcdefg.apps.googleusercontent.com"
   googleClientId: "",
-  // 보안상 서버 검증을 할 수 없는 GitHub Pages의 무료 정적 방식입니다.
-  // 첫 Google 로그인 계정을 이 브라우저의 관리자 계정으로 등록합니다.
-  // 다른 브라우저/기기와 관리자 권한을 공유하지 않습니다.
+  // Google 계정 이메일의 @ 앞부분이 egiby07인 계정만 관리자 후보로 허용합니다.
+  // 정확한 이메일 주소로 제한하려면 adminEmail을 사용하세요.
+  adminEmail: "",
+  adminEmailPrefix: "egiby07",
   localAdminBinding: true
 };
